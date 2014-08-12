51f09bf336a197ba60030765dac3c8ad3c662439
Jesse Wolgamott <jesse@comalproductions.com>
2014-08-12 11:45:02 -0500

    initial cart

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,20 @@ class HomeController < ApplicationController
   def index
     @books = Book.all
   end
+
+  def cart
+    # get books from cart
+    # show the books
+  end
+
+  def add_to_cart
+    # find book from params[:book_id]
+    book = Book.find(params[:book_id])
+
+    # add to cart
+
+    # redirect_to show cart page
+    redirect_to show_cart_path
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'home/index'
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
 
   root to: "home#index"
-  
-
+  post "/cart/:book_id" => "home#add_to_cart", as: :add_to_cart
+  get "/cart" => "home#cart", as: :show_cart
 end
